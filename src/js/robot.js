999c466e8427a55d86744b5b2760bac7bf22ddd1
Mallerie Shirley <mallerie@gmail.com>
2016-11-12 15:43:41 -0500

    Added robot class

---
+++ src/js/robot.js
@@ -0,0 +1,16 @@
+class Robot {
+  constructor(name, avatar, power, defence) {
+    this.name = name;
+    this.avatar = avatar;
+    this.power = power;
+    this.defence = defence;
+  }
+
+  fight() {
+
+  }
+
+
+}
+
+export { Robot };
