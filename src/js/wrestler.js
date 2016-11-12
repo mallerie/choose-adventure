d63cd303303677ee4ac3c843246a652492cb554e
Mallerie Shirley <mallerie@gmail.com>
2016-11-12 15:45:02 -0500

    Added wrestler class

---
+++ src/js/wrestler.js
@@ -0,0 +1,16 @@
+class Wrestler {
+  constructor(name, avatar, power, defence) {
+    this.name = name;
+    this.avatar = avatar;
+    this.power = power;
+    this.defence = defence;
+  }
+
+  fight() {
+
+  }
+
+
+}
+
+export { Wrestler };
